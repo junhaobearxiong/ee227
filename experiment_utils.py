3c6aacb2b65f0008eda4e29d71bb310cdd90825e
junhaobearxiong <jxiong3@jhu.edu>
2021-11-17 14:12:23 -0800

    fix some alpha stuff

--- experiment_utils.py
+++ experiment_utils.py
@@ -83,7 +83,7 @@ def run_lasso_across_sample_sizes(X, y, num_samples_arr, savefile, alpha=None, n
             alphas_list = [5e-8, 1e-8, 5e-7, 1e-7, 5e-6, 1e-6, 5e-5, 1e-5, 5e-4, 1e-4, 5e-3, 1e-3, 5e-2, 1e-2, 5e-1, 1e-1, 1]
             samples_idx = np.random.choice(np.arange(X_train.shape[0]), n, replace=False)
             model = LassoCV(alphas=alphas_list, n_jobs=10).fit(X_train[samples_idx, :], y_train[samples_idx])
-            alphas[i, j] = model.alpha_          
+            alphas[i, j] = model.alpha_    
             '''
 
             # evaluating on test set
--- scripts/experiment_poelwijk_lasso.py
+++ scripts/experiment_poelwijk_lasso.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('num_replicates', type=int, nargs='?', default=1)
 parser.add_argument('max_num_samples', type=int, nargs='?', default=1000)
 parser.add_argument('num_samples_step', type=int, nargs='?', default=100)
+parser.add_argument('alpha', type=float, nargs='?', default=None)
 args = parser.parse_args()
 
 start_time = datetime.now()
@@ -15,9 +16,9 @@ X, y = poelwijk_construct_Xy()
 num_samples_arr = np.arange(100, args.max_num_samples, args.num_samples_step)
 run_lasso_across_sample_sizes(
 	X, y,
-	alpha=1e-5,
+	alpha=args.alpha,
 	num_samples_arr=num_samples_arr,
 	num_replicates=args.num_replicates,
-	savefile='results/poelwijk_lasso_sample_sizes_r{}_n{}s{}.pkl'.format(args.num_replicates, args.max_num_samples, args.num_samples_step)
+	savefile='results/poelwijk_lasso_sample_sizes_r{}_n{}s{}_alpha{}.pkl'.format(args.num_replicates, args.max_num_samples, args.num_samples_step, args.alpha)
 )
 print('Time elapsed: {}'.format(datetime.now() - start_time))
