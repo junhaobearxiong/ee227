f199005c6ff682ee1a8d4269a595dd74933fd1b1
junhaobearxiong <jxiong3@jhu.edu>
2021-11-16 19:42:29 -0800

    add step

--- scripts/experiment_poelwijk_lasso.py
+++ scripts/experiment_poelwijk_lasso.py
@@ -7,15 +7,16 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('num_replicates', type=int, nargs='?', default=1)
 parser.add_argument('max_num_samples', type=int, nargs='?', default=1000)
+parser.add_argument('num_samples_step', type=int, nargs='?', default=100)
 args = parser.parse_args()
 
 start_time = datetime.now()
 X, y = poelwijk_construct_Xy()
-num_samples_arr = np.arange(100, args.max_num_samples, 200)
+num_samples_arr = np.arange(100, args.max_num_samples, args.num_samples_step)
 run_lasso_across_sample_sizes(
 	X, y,
 	num_samples_arr=num_samples_arr,
 	num_replicates=args.num_replicates,
-	savefile='results/poelwijk_lasso_sample_sizes_r{}_n{}.pkl'.format(args.num_replicates, args.max_num_samples)
+	savefile='results/poelwijk_lasso_sample_sizes_r{}_n{}s{}.pkl'.format(args.num_replicates, args.max_num_samples, args.num_samples_step)
 )
 print('Time elapsed: {}'.format(datetime.now() - start_time))
