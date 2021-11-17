dd5fe95c589f78753e77e5b90ee4c82120665b6b
junhaobearxiong <jxiong3@jhu.edu>
2021-11-16 19:07:56 -0800

    add cmd line num replicates

--- experiments/poelwijk_lasso_experiments.py
+++ experiments/poelwijk_lasso_experiments.py
@@ -17,6 +17,7 @@ num_samples_arr = np.arange(100, args.max_num_samples, 200)
 run_lasso_across_sample_sizes(
 	X, y,
 	num_samples_arr=num_samples_arr,
+	num_replicates=args.num_replicates,
 	savefile='results/poelwijk_lasso_sample_sizes_r{}_n{}.pkl'.format(args.num_replicates, args.max_num_samples)
 )
 print('Time elapsed: {}'.format(datetime.now() - start_time))
